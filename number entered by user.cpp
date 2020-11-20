6f2204318f504bfd4a6513d78dfa29f43bf06b90
Nikhil-Rane108 <71148109+Nikhil-Rane108@users.noreply.github.com>
2020-11-20 18:22:46 +0530

    Update number entered by user.cpp

--- number entered by user.cpp
+++ number entered by user.cpp
@@ -1,11 +1,10 @@
-#include<iostream.h>
-#include<conio.h>
-void main()
-{
-     float a;
-     clrscr();
-     cout<<"Enter number:";
-     cin>>a;
-     cout<<a;
-getch();
-}     
+#include<iostream>
+using namespace std;
+int main()
+{ 
+   int num;
+   cout<<"Enter a number:";
+   cin>>num;
+   cout<<"The Entered number is "<<num;
+   return 0;
+ }
